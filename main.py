0262d18f1cf1c88931d528ce88638b6122af1a5f
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-12-08 19:47:18 -0500

    Implement webpage for displaying the API response.

--- main.py
+++ main.py
@@ -14,4 +14,19 @@ params = {
 response = requests.get(endpoint, params)
 content = response.json()
 
-print(content)
+img_url = content["url"]
+img_title = content["title"]
+img_description = content["explanation"]
+img_copyright = content["copyright"] if "copyright" in content else ""
+
+# Web App
+st.set_page_config(img_title + " | " + api_title)
+
+st.title(api_title)
+st.image(img_url)
+
+if img_copyright:
+    st.caption("Copyright: " + img_copyright)
+
+st.header(img_title)
+st.text(img_description)
